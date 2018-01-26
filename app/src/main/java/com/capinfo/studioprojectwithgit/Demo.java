451197c9842d4599824c3f9a250c27063a444445
luhongwei <845826379@qq.com>
2018-01-26 17:27:03 +0800

    增加一个打印方法

--- app/src/main/java/com/capinfo/studioprojectwithgit/Demo.java
+++ app/src/main/java/com/capinfo/studioprojectwithgit/Demo.java
@@ -1,9 +1,15 @@
 package com.capinfo.studioprojectwithgit;
 
+import android.util.Log;
+
 /**
  * Created by luhongwei on 2018/1/26.
  * 增加注释，这是一个demo,用来学习git
  */
 
 public class Demo {
+
+    public static void printLnHello(){
+        Log.e("wei","hello world!");
+    }
 }
